9775022879eea7bf8ac9013347eaa463281e5f7c
Rohima khanom <140314519+Rohima-77@users.noreply.github.com>
2025-06-05 22:21:10 +0600

    Create RunnableInterface.java

---
+++ RunnableInterface.java
@@ -0,0 +1,10 @@
+class MyRunnable implements Runnable {
+    public void run() {
+        System.out.println("Runnable thread is running.");
+    }
+
+    public static void main(String[] args) {
+        Thread t = new Thread(new MyRunnable());
+        t.start();  // Starts the thread
+    }
+}
